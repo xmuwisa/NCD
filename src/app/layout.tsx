f4d41af446d822f61a2430c653d554c86f6b4c28
Luisa Eustaquio <95476379+xmuwisa@users.noreply.github.com>
2024-09-27 16:54:52 +0800

    Add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link
+                    rel="icon"
+                    href="/images/logo/White.png"
+                    type="image/png"
+                />
+            </head>
             <body>
                 <Header />
                 {children}
